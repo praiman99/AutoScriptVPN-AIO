c0ef250e603ab9814c52c1dc8228461eea9c8fe9
PR Aiman <58582776+praiman99@users.noreply.github.com>
2024-03-15 21:29:05 +0800

    Delete bot directory

--- bot/__main__.py
+++
@@ -1,9 +0,0 @@
-from kyt import *
-from importlib import import_module
-from kyt.modules import ALL_MODULES
-for module_name in ALL_MODULES:
-        imported_module = import_module("kyt.modules." + module_name)
-bot.run_until_disconnected()
-
-usr.local.bin
-
